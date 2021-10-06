2242a6666fe2af0515a5d2a9d3715da3fe51c883
albamr09 <bertaalba1@gmail.com>
2021-10-06 13:56:16 +0200

    Modify script to test locally

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 #docker-compose -f docker-compose.dev.yml build --no-rm --no-cache
 #docker-compose -f docker-compose.dev.yml up --force-recreate -d --remove-orphans 
-docker-compose -f docker-compose.dev.yml run app sh -c "npm test"
+docker-compose -f docker-compose.dev.yml run app sh -c "npm run localTest"
