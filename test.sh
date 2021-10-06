ffb5d5fdcde3759e67753a137433903fd139391c
albamr09 <bertaalba1@gmail.com>
2021-10-06 17:22:44 +0200

    Remove test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-#docker-compose -f docker-compose.dev.yml build --no-rm --no-cache
-#docker-compose -f docker-compose.dev.yml up --force-recreate -d --remove-orphans 
-docker-compose -f docker-compose.dev.yml run app sh -c "npm run localTest"
