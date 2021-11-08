6593645bf2ebc58cda43dc2d4fef87ab3038b8bc
albamr09 <bertaalba1@gmail.com>
2021-11-08 21:12:14 +0100

    [workflow] Change test

--- scripts/createSecrets.sh
+++ scripts/createSecrets.sh
@@ -7,12 +7,14 @@ set -o allexport
 source $ENV_DIR
 set +o allexport
 
+REL_SECRETS_DIR="../"$SECRETS_DIR
+
 # Create secrets
-echo $DB_ROOT_USER > $SECRETS_DIR"/db_root_user"
-echo $DB_ROOT_PASSWORD > $SECRETS_DIR"/db_root_passwd"
-echo $DB_USER > $SECRETS_DIR"/db_user"
-echo $DB_PASSWORD > $SECRETS_DIR"/db_passwd"
-echo $TOKEN_SECRET > $SECRETS_DIR"/token_secret"
-echo $DB_URI > $SECRETS_DIR"/db_uri"
-echo $DB_NAME > $SECRETS_DIR"/db_name"
+echo $DB_ROOT_USER > $REL_SECRETS_DIR"/db_root_user"
+echo $DB_ROOT_PASSWORD > $REL_SECRETS_DIR"/db_root_passwd"
+echo $DB_USER > $REL_SECRETS_DIR"/db_user"
+echo $DB_PASSWORD > $REL_SECRETS_DIR"/db_passwd"
+echo $TOKEN_SECRET > $REL_SECRETS_DIR"/token_secret"
+echo $DB_URI > $REL_SECRETS_DIR"/db_uri"
+echo $DB_NAME > $REL_SECRETS_DIR"/db_name"
 
