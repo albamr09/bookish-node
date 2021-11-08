7ce41aac47dbcfd5af5505a3838afb1d659396b3
albamr09 <bertaalba1@gmail.com>
2021-11-08 21:17:29 +0100

    [workflow] Change test

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,6 @@
 
 ./createSecrets.sh
 
-docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env.dev" down
-docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env.dev" up
-docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env.dev" up -d
+docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" down
+docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" up
+docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" up -d
