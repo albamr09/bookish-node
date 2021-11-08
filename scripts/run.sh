0dac32b494e5a7298e40186c88ccdce5f275a0ed
albamr09 <bertaalba1@gmail.com>
2021-11-08 21:20:50 +0100

    [workflow] Change test

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@
 
 docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" down
 docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" up
-docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" up -d
+# docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" up -d
