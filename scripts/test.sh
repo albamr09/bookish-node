033f5d31d6f700b11d3928f69d64f2797cc20f22
albamr09 <bertaalba1@gmail.com>
2021-11-08 20:58:29 +0100

    [workflow] Change test

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,3 +4,4 @@
 
 docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" down
 docker-compose -f "../docker-compose.yml" --env-file "../config/.env/.env" run app sh -c "npm test && npm run lint"
+# docker exec bookish_node sh -c "npm test && npm run lint"
